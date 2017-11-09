45f78029850f6038cd92fc8f223e867359d0fbec
Dipayan94 <dipayan4.das@gmail.com>
2017-11-09 15:42:11 +0530

    Adding files to local git repo

---
+++ in.demo.auto/src/test/java/in/demo/auto/Test1.java
@@ -0,0 +1,12 @@
+package in.demo.auto;
+
+import org.testng.annotations.Test;
+
+public class Test1 
+{
+	@Test
+	public void m()
+	{
+		System.out.println("Hi Dipayan Das, Welcome to selenium");
+	}
+}
---
+++ in.demo.auto/src/test/java/in/demo/auto/Test2.java
@@ -0,0 +1,14 @@
+package in.demo.auto;
+
+import org.testng.annotations.Test;
+
+public class Test2 
+{
+	
+	@Test
+	public void ne()
+	{
+		System.out.println("New Class Deployed");
+	}
+
+}
